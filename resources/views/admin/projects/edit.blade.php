23189070cbe94c227049b77247d5ea464ce0c457
EsterSPR <web.esterspreafico@gmail.com>
2023-03-03 19:10:47 +0100

    customizzati form edit e create

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -1,75 +1,59 @@
-<!-- @extends('layouts.admin')
+@extends('layouts.admin')
 @section('content')
-<div class="container text-white">
+<div class="container">
     <div class="row">
         <div class="col-12 text-center m-4">
-            <h2 class="text-white">Modifica questo movies</h2>
+            <h2>Edit project</h2>
         </div>
         <div class="col-12">
-            <form action="{{route('admin.movies.store')}}" method="POST">
+            <form action="{{route('admin.projects.update', $project->id)}}" method="POST">
             @csrf
-            <div class="form-group mb-3">
-                <label class="control-label mb-2">
-                    Titolo
-                </label>
-                <input type="text" class="form-control" placeholder="Titolo" id="title" name ="title">
-                    @error('title')
-                    <div class="alert alert-danger mt-2">{{$message}}</div>
-                    @enderror
-            </div>
-            <div class="form-group mb-3">
-                <label class="control-label mb-2">
-                    Original Titolo
-                </label>
-                <input type="text" class="form-control" placeholder="Titolo" id="original_title" name ="original_title">
-                    @error('original_title')
-                    <div class="alert alert-danger mt-2">{{$message}}</div>
-                    @enderror
-            </div>
-            <div class="form-group mb-3">
-                <label class="control-label mb-2">
-                    Vote
-                </label>
-                <input type="number" class="form-control" placeholder="Titolo" id="vote" name ="vote">
-                    @error('vote')
-                    <div class="alert alert-danger mt-2">{{$message}}</div>
-                    @enderror
-            </div>
-            <div class="form-group mb-3">
-                <label class="control-label mb-2">
-                    Cover
-                </label>
-                <input type="text" class="form-control" placeholder="Titolo originale" id="cover_path" name ="cover_path">
-                    @error('cover_path')
-                    <div class="alert alert-danger mt-2">{{$message}}</div>
-                    @enderror
-            </div>
-            <div class="form-group mb-3">
-                <label class="control-label mb-2">
-                    Data
-                </label>
-                <input type="date" class="form-control" placeholder="Data" name="release_date">
-                    @error('release_date')
+            @method('PUT')
+                <div class="form-group mb-3">
+                    <label class="control-label mb-2">
+                        Project title
+                    </label>
+                    <input type="text" class="form-control es_yellowborder" placeholder="Project title" id="project_title" name="project_title" value="{{old('project_title') ?? $project->project_title}}">
+                        @error('project_title')
                         <div class="alert alert-danger mt-2">{{$message}}</div>
-                    @enderror
-            </div>
-            <div class="form-group mb-3">
-                <label class="control-label mb-2">
-                    Cast
-                </label>
-                <textarea type="text-area" class="form-control" placeholder="Cast" id="cast" name ="cast"></textarea>
-            </div>
-            <div class="form-group mb-3">
-                <label class="control-label mb-2">
-                    Nazionalità
-                </label>
-                <textarea type="text-area" class="form-control" placeholder="Nazionalità" id="nationality" name ="nationality"></textarea>
-            </div>
-            <div class="form-group mb-3">
-                <button type="submit" class="btnblue">Salva</button>
-            </div>
+                        @enderror
+                </div>
+
+                <div class="form-group mb-3">
+                    <label class="control-label mb-2">
+                        Cover image url
+                    </label>
+                    <input type="text" class="form-control es_yellowborder" placeholder="Cover image url" id="cover_img" name="cover_img" value="{{old('cover_img') ?? $project->cover_img}}">
+                        @error('cover_img')
+                        <div class="alert alert-danger mt-2">{{$message}}</div>
+                        @enderror
+                </div>
+
+                <div class="form-group mb-3">
+                    <label class="control-label mb-2">
+                        Project slug
+                    </label>
+                    <input type="text" class="form-control es_yellowborder" placeholder="Project slug" id="slug" name ="slug" value="{{old('slug') ?? $project->slug}}">
+                        @error('slug')
+                        <div class="alert alert-danger mt-2">{{$message}}</div>
+                        @enderror
+                </div>
+
+                <div class="form-group mb-3">
+                    <label class="control-label mb-2">
+                        Project description
+                    </label>
+                    <textarea name="project_desc" id="project_desc" placeholder="Project description" value="{{old('project_desc') ?? $project->project_desc}}" class="w-100 es_yellowborder rounded p-2"></textarea>
+                        @error('project_desc')
+                        <div class="alert alert-danger mt-2">{{$message}}</div>
+                        @enderror
+                </div>
+
+                <div class="form-group mb-3">
+                    <button type="submit" class="btn es_button mt-2">Salva</button>
+                </div>
             </form>
         </div>
     </div>
 </div>
-@endsection -->
+@endsection
